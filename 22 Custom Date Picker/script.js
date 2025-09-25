9cfca889eec999ac054caa516ccc6c76f8ee63d4
Sambhav <sambhav12wakhariya@gmail.com>
2025-09-25 23:30:00 +0530

    Checked In : Date Picker Previous And Next Button Completed

--- 22 Custom Date Picker/script.js
+++ 22 Custom Date Picker/script.js
@@ -1,19 +1,23 @@
-import {format, fromUnixTime, getUnixTime} from 'date-fns';
+import {addMonths, format, fromUnixTime, getUnixTime, subMonths} from 'date-fns';
 
 const datePickerButton = document.querySelector('.date-picker-button');
 const datePicker = document.querySelector('.date-picker');
 const datePickerHeaderText = document.querySelector('.current-month')
+const previousMonthButton = document.querySelector('.prev-month-button');
+const nextMonthButton = document.querySelector('.next-month-button');
+let currentDate = new Date();
 
 datePickerButton.addEventListener("click", e=>{
     datePicker.classList.toggle('show');
     const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate)
+    currentDate = selectedDate;
     setupDatePicker(selectedDate);
 });
 
 
 // Simple Function to Setup our Date Picker
 function setupDatePicker(selectedDate){
-    datePickerHeaderText.innerHTML = format(selectedDate, 'MMMM - yyyy');
+    datePickerHeaderText.innerHTML = format(currentDate, 'MMMM - yyyy');
 }
 
 // Simple Function to Set Date on the Button
@@ -22,5 +26,17 @@ function setDate(date){
     datePickerButton.dataset.selectedDate = getUnixTime(date);
     return;
 }
+
+// Next Click
+nextMonthButton.addEventListener('click', ()=>{
+    currentDate = addMonths(currentDate, 1);
+    setupDatePicker();
+})
+
+// On Previous Click
+previousMonthButton.addEventListener('click', ()=>{
+    currentDate = subMonths(currentDate, 1);
+    setupDatePicker();
+})
 
 setDate(new Date());
