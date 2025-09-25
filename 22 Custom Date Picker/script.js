b52eb92dcb5656db82181235381f5cbed7936a9a
Sambhav <sambhav12wakhariya@gmail.com>
2025-09-25 23:30:00 +0530

    Checked In : Date Picker Header Date Formated

--- 22 Custom Date Picker/script.js
+++ 22 Custom Date Picker/script.js
@@ -1,15 +1,26 @@
-import {format} from 'date-fns';
+import {format, fromUnixTime, getUnixTime} from 'date-fns';
 
 const datePickerButton = document.querySelector('.date-picker-button');
 const datePicker = document.querySelector('.date-picker');
+const datePickerHeaderText = document.querySelector('.current-month')
 
 datePickerButton.addEventListener("click", e=>{
     datePicker.classList.toggle('show');
+    const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate)
+    setupDatePicker(selectedDate);
 });
 
-setDate(new Date());
 
+// Simple Function to Setup our Date Picker
+function setupDatePicker(selectedDate){
+    datePickerHeaderText.innerHTML = format(selectedDate, 'MMMM - yyyy');
+}
+
+// Simple Function to Set Date on the Button
 function setDate(date){
-    datePickerButton.innerHTML = format(date, '	MMMM Mo, yyyy')
+    datePickerButton.innerHTML = format(date, '	MMMM do, yyyy')
+    datePickerButton.dataset.selectedDate = getUnixTime(date);
     return;
 }
+
+setDate(new Date());
