4b23eb9381842f356cb759c91124aaabc9d7bd55
Sambhav <sambhav12wakhariya@gmail.com>
2025-09-25 23:30:00 +0530

    Checked In : Completed With the Custom Date Picker

--- 22 Custom Date Picker/script.js
+++ 22 Custom Date Picker/script.js
@@ -1,10 +1,11 @@
-import {addMonths, format, fromUnixTime, getUnixTime, subMonths} from 'date-fns';
+import {addMonths, eachDayOfInterval, endOfMonth, endOfWeek, format, fromUnixTime, getUnixTime, isSameDay, isSameMonth, startOfMonth, startOfWeek, subMonths} from 'date-fns';
 
 const datePickerButton = document.querySelector('.date-picker-button');
 const datePicker = document.querySelector('.date-picker');
 const datePickerHeaderText = document.querySelector('.current-month')
 const previousMonthButton = document.querySelector('.prev-month-button');
 const nextMonthButton = document.querySelector('.next-month-button');
+const dateGrid = document.querySelector('.date-picker-grid-dates');
 let currentDate = new Date();
 
 datePickerButton.addEventListener("click", e=>{
@@ -18,6 +19,35 @@ datePickerButton.addEventListener("click", e=>{
 // Simple Function to Setup our Date Picker
 function setupDatePicker(selectedDate){
     datePickerHeaderText.innerHTML = format(currentDate, 'MMMM - yyyy');
+    setupDates(selectedDate);
+}
+
+// This will setup all the dates between that particular month
+function setupDates(selectedDate){
+    const firstWeekStart = startOfWeek(startOfMonth(currentDate));
+    const lastWeekEnd = endOfWeek(endOfMonth(currentDate));
+    const dates = eachDayOfInterval({start : firstWeekStart, end : lastWeekEnd});
+    dateGrid.innerHTML = "";
+    dates.forEach(date =>{
+        const dateElement = document.createElement('button');
+        dateElement.innerHTML = date.getDate();
+        dateElement.classList.add('date');
+
+        if(!isSameMonth(date, currentDate)){
+            dateElement.classList.add('date-picker-other-month-date');
+        }
+
+        if(isSameDay(date, selectedDate)){
+            dateElement.classList.add('selected');
+        }
+
+        dateElement.addEventListener('click', ()=>{
+            setDate(date);
+            datePicker.classList.remove('show');
+        })
+
+        dateGrid.appendChild(dateElement);
+    })
 }
 
 // Simple Function to Set Date on the Button
@@ -29,14 +59,16 @@ function setDate(date){
 
 // Next Click
 nextMonthButton.addEventListener('click', ()=>{
+    const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate)
     currentDate = addMonths(currentDate, 1);
-    setupDatePicker();
+    setupDatePicker(selectedDate);
 })
 
 // On Previous Click
 previousMonthButton.addEventListener('click', ()=>{
+    const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate)
     currentDate = subMonths(currentDate, 1);
-    setupDatePicker();
+    setupDatePicker(selectedDate);
 })
 
 setDate(new Date());
